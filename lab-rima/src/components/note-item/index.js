c3809cb6082ac10b9829b2fd3016f306930d64bc
Rima <rmhrok@gmail.com>
2018-02-22 05:34:53 +0000

    get delete button working

--- lab-rima/src/components/note-item/index.js
+++ lab-rima/src/components/note-item/index.js
@@ -4,6 +4,18 @@ import React from 'react';
 class NoteItem extends React.Component{
   constructor(props){
     super(props);
+
+    let memberFunctions = Object.getOwnPropertyNames(NoteItem.prototype);
+    for(let functionName of memberFunctions){
+      if(functionName.startsWith('handle')){
+        this[functionName] = this[functionName].bind(this);
+      }
+    }
+  }
+
+  handleClick(event) {
+    event.preventDefault();
+    this.props.handleRemoveNote(this.props.note);
   }
 
   render(){
@@ -13,7 +25,7 @@ class NoteItem extends React.Component{
         <p>{this.props.note.content}</p>
         <button
           className="delete"
-          onClick={this.props.handleRemoveNote(this.props.note)}>
+          onClick={this.handleClick}>
           Delete
         </button>
       </li>
