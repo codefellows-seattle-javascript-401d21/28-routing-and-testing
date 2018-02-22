29432733dc74bc4306abb987aece35064cd7f70d
Steven Carpenter <stevegcarpenter@gmail.com>
2018-02-21 17:38:40 -0800

    Adding webpack file

---
+++ lab-steve/webpack.config.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: `${__dirname}/src/main.js`,
+  output: {
+    filename: 'bundle.[hash].js',
+    path: `${__dirname}/build`,
+  },
+  plugins: [
+    new HtmlPlugin(),
+    new ExtractTextPlugin('bundle-[hash].css'),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader']),
+      },
+    ],
+  },
+  devtool: 'eval-source-map',
+  devServer: {
+    historyApiFallback: true,
+  },
+};
