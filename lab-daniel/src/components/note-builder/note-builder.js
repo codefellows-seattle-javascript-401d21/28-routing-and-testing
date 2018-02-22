dcfbbabda62e4f172bd3658743557156edffb6f3
daniellogerstedt <daniel.logerstedt@gmail.com>
2018-02-21 16:02:56 -0800

    fixed formatting to make builder more readable

--- lab-daniel/src/components/note-builder/note-builder.js
+++ lab-daniel/src/components/note-builder/note-builder.js
@@ -7,7 +7,15 @@ class NoteBuilder extends React.Component {
 
   render() {
     let { title, content, id } = this.props.note;
-    return (<li className="note"><section><h2>{title}</h2><p>{content}</p></section><button id={id} onClick={this.props.remove}>Remove</button></li>);
+    return (
+      <li className="note">
+        <section>
+          <h2>{title}</h2>
+          <p>{content}</p>
+        </section>
+        <button id={id} onClick={this.props.remove}>Remove</button>
+      </li>
+    );
   }
 }
 
