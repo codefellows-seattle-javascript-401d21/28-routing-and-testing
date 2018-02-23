4890365e5c4f3f8cfc7bcfdd63443d5129406525
Ed Abrahamsen <esabrahamsen@yahoo.com>
2018-02-22 18:52:03 -0800

    finish

--- lab-ed/src/__test__/dashboard.test.js
+++ lab-ed/src/__test__/dashboard.test.js
@@ -4,12 +4,12 @@ import Adapter from 'enzyme-adapter-react-16'
 require('jest')
 
 Enzyme.configure({adapter: new Adapter()})
-import Dashboard from '../component/dashboard'
+import Dashboard from '../components/dashboard/index'
 
 describe('Dashboard', () => {
   test('Testing initial state', () => {
     let mountedDashboard = Enzyme.mount(<Dashboard />)
 
-    expect(mountedDashboard.state('expenses')).toEqual([])
+    expect(mountedDashboard.state('notes')).toEqual([])
   })
 })
--- lab-ed/src/components/app/index.js
+++ lab-ed/src/components/app/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import {BrowserRouter,Route,Link} from 'react-router-dom'
-import Dashboard from '../dashboard/index'
+import Dashboard from '../dashboard/index.js'
+import Landing from '../landing/index.js'
 
 class App extends React.Component{
-  render(){
+  render() {
     return(
       <div className='app'>
+        <h1>A note taking app</h1>
+        <nav>
+          <button><a href="/">Home</a></button>
+          <button><a className='addNote' href="/dashboard">Add a note</a></button>
+        </nav>
         <BrowserRouter>
           <div>
-            <Route exact path='/' component={() => <h1>Home!</h1>}/>
+            <Route exact path='/' component={Landing}/>
             <Route exact path='/dashboard' component={Dashboard}/>
           </div>
         </BrowserRouter>
@@ -16,5 +22,4 @@ class App extends React.Component{
     )
   }
 }
-
 export default App
